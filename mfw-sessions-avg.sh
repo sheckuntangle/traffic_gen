1c3bb5be8ebf0692a11330bea7deda4f07fa9e06
sheckuntangle <sheck@untangle.com>
2022-01-13 17:13:04 -0500

    script changes

--- mfw-sessions-avg.sh
+++ mfw-sessions-avg.sh
@@ -1,5 +1,16 @@
 #!/bin/ash
 
+# set folder parameter 
+folder=$1
+
+# check for missing variables
+if [ -z $1 ]; then
+    echo "Missing parameter, please a name for summary file, ex:"
+    echo "./mfw-sessions test_file"
+    echo "The command above will output the summary to the screen and to /tmp/test_file.summary"
+    exit
+fi
+
 # variables
 start_time=$(sed '2q;d' /tmp/session_count_dates.log)
 end_time=$(sed 'x;$!d' /tmp/session_count_dates.log)
@@ -13,17 +24,21 @@ load_count=$(grep "Load Averages" /tmp/session_count_dates.log | wc -l)
 load_average=$(echo "scale=4; $load_total/$load_count" | bc -l)
 
 # echo title Session Summary
-echo "Session Summary"
+echo "Session Summary" | tee -a /tmp/$folder.summary
 
 # echo times
-echo "Start Time: $start_time"
-echo "End Time: $end_time"
-echo "Elapsed Time: $load_count minutes"
+echo "Start Time: $start_time" | tee -a /tmp/$folder.summary
+echo "End Time: $end_time" | tee -a /tmp/$folder.summary
+echo "Elapsed Time: $load_count minutes" | tee -a /tmp/$folder.summary
 
 # echo session counts
-echo "High Session Count: $high_sessions"
-echo "Low Session Count: $low_sessions"
-echo "Average Session Count: $average_sessions"
+echo "High Session Count: $high_sessions" | tee -a /tmp/$folder.summary
+echo "Low Session Count: $low_sessions" | tee -a /tmp/$folder.summary
+echo "Average Session Count: $average_sessions" | tee -a /tmp/$folder.summary
 
 # echo load average
-echo "CPU Load Average: $load_average"
+echo "CPU Load Average: $load_average" | tee -a /tmp/$folder.summary
+
+# reset session_count.log and sessions_count_dates.log
+echo >> /tmp/session_count.log
+echo >> /tmp/session_count_dates.log
